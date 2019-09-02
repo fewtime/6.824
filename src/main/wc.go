3c581a41392dc1022cfd22dad8b7379858879fad
cowlog <cowlog.li@gmail.com>
2019-09-02 17:10:41 +0800

    [add] Lab 1, Part 2

--- src/main/wc.go
+++ src/main/wc.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"mapreduce"
 	"os"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 //
@@ -15,6 +18,20 @@ import (
 //
 func mapF(filename string, contents string) []mapreduce.KeyValue {
 	// Your code here (Part II).
+
+	// 划分单词
+	words := strings.FieldsFunc(contents, func(r rune) bool {
+		return !unicode.IsLetter(r)
+	})
+
+	var kvs []mapreduce.KeyValue
+	for _, word := range words {
+		kvs = append(kvs, mapreduce.KeyValue{
+			Key:   word,
+			Value: "1",
+		})
+	}
+	return kvs
 }
 
 //
@@ -24,6 +41,7 @@ func mapF(filename string, contents string) []mapreduce.KeyValue {
 //
 func reduceF(key string, values []string) string {
 	// Your code here (Part II).
+	return strconv.Itoa(len(values))
 }
 
 // Can be run in 3 ways:
